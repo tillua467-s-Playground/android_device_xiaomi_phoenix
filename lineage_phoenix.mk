9571b209758479a2c55d46e328b73def4c2b28b1
tillua467 <tillua467@gmail.com>
2025-05-21 22:45:58 +0600

    phoenix: BringUp!: Let's Build Matrixx OS!

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -6,21 +6,40 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+WITH_GMS := true
+WITH_GMS_COMMS_SUITE := true
+TARGET_SUPPORTS_OMX_SERVICE := false
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_FINGERPRINT_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_STOCK_AICORE := true
+TARGET_SUPPORTS_WALLEFFECT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
-
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
+TARGET_VENDOR_PRODUCT_NAME := phoenix
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
